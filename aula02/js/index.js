f8fea7ff8d70fd756a7cb4fbb507573d44929770
biadecolo <biancadecolo@gmail.com>
2024-07-16 21:58:08 -0300

    desafio complementar aula 02 adicionado

--- aula02/js/index.js
+++ aula02/js/index.js
@@ -15,6 +15,7 @@ document.querySelector('p').innerHTML = textoFinal
 alert(textoFinal)
 */
 
+/* 
 var n1 = parseInt(prompt('Número 1'));
 var n2 = parseInt(prompt('Número 2'));
 
@@ -25,3 +26,13 @@ var frase = "A soma é " + soma + " e o produto é " + produto;
 
 console.log(frase);
 document.querySelector('p').innerHTML = frase;
+*/
+
+var ano = parseInt(prompt("Em que ano estamos?"));
+var anoNascimento = parseInt(prompt("Em que você nasceu?"));
+var nome = prompt("Digite aqui o seu nome"); 
+
+var idadeAtual = ano - anoNascimento;
+var fraseFinal = "Entendi. O seu nome é " + nome + ", você nasceu em " + anoNascimento + "e sua idade atualmente é " + idadeAtual + ", certo?"
+
+document.querySelector('p').innerHTML = fraseFinal;
