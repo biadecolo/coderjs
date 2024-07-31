3bcb991807eaca8296319b658bba2227925d4c2c
biadecolo <biancadecolo@gmail.com>
2024-07-31 07:56:58 -0300

    aula 06

---
+++ aula06/js/index.js
@@ -0,0 +1,36 @@
+/*var numero = Number(prompt("Digite um número (menor ou igual a 10):"));
+var i = 1;
+
+if (numero <= 10) {
+    while (i <= numero) {
+        console.log(i); // Imprima o valor de i (números de 1 até o número inserido)
+        i++;
+    }
+} else {
+    document.getElementById('meuParagrafo').innerHTML = ("Por favor, insira um número menor ou igual a 10.");
+}
+*/
+
+function calcularFatorial(numero) {
+    if (numero < 0) {
+        return "O fatorial não está definido para números negativos.";
+    } else if (numero === 0 || numero === 1) {
+        return 1;
+    } else {
+        let fatorial = 1;
+        let i = 1;
+
+        while (i <= numero) {
+            fatorial *= i;
+            i++;
+        }
+
+        return fatorial;
+    }
+}
+
+// Exemplo de uso
+const numeroInserido = parseInt(prompt("Digite um número para calcular o fatorial:"));
+const resultado = calcularFatorial(numeroInserido);
+
+document.getElementById('meuParagrafo').innerHTML = (`O fatorial de ${numeroInserido} é ${resultado}.`);
