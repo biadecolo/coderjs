156720856d403e1d4ec5feaf9feb9fc790886eca
biadecolo <biancadecolo@gmail.com>
2024-08-05 07:29:28 -0300

    mudanças no index

--- aula06/js/index.js
+++ aula06/js/index.js
@@ -11,12 +11,16 @@ if (numero <= 10) {
 }
 */
 
-function calcularFatorial(numero) {
-    if (numero < 0) {
+function calcularFatorial(numero) { //já recebe o número na função
+    if (numero < 0) { //trata o número para o caso de ser -0
         return "O fatorial não está definido para números negativos.";
-    } else if (numero === 0 || numero === 1) {
+    } 
+    
+    else if (numero === 0 || numero === 1) {
         return 1;
-    } else {
+    } 
+    
+    else {
         let fatorial = 1;
         let i = 1;
 
@@ -29,7 +33,6 @@ function calcularFatorial(numero) {
     }
 }
 
-// Exemplo de uso
 const numeroInserido = parseInt(prompt("Digite um número para calcular o fatorial:"));
 const resultado = calcularFatorial(numeroInserido);
 
